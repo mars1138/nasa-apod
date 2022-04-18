b014b1eece58d4a5475ca0ef35ec0ca6c247ee3e
LearnToCode1138 <mdelgado1138@hotmail.com>
2022-04-17 21:04:55 -0700

    fix cardlist

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,25 +3,27 @@ import Card from './Card';
 const CardList = (props) => {
   const { apodList, saveFavorite, home, removeFavorite, favorites } = props;
 
+  const filteredApodList = apodList.filter(
+    (apod) => apod.media_type === 'image'
+  );
+
   return (
     <div className="flex flex-wrap justify-center w-80-l ml-auto mr-auto">
-      {apodList.map((apod, i) => {
-        if (apod.media_type === 'image') {
-          return (
-            <Card
-              key={i}
-              title={apod.title}
-              date={apod.date}
-              desc={apod.explanation}
-              url={apod.url}
-              hdurl={apod.hdurl}
-              saveFavorite={saveFavorite}
-              removeFavorite={removeFavorite}
-              favorites={favorites}
-              home={home}
-            />
-          );
-        }
+      {filteredApodList.map((apod, i) => {
+        return (
+          <Card
+            key={i}
+            title={apod.title}
+            date={apod.date}
+            desc={apod.explanation}
+            url={apod.url}
+            hdurl={apod.hdurl}
+            saveFavorite={saveFavorite}
+            removeFavorite={removeFavorite}
+            favorites={favorites}
+            home={home}
+          />
+        );
       })}
     </div>
   );
